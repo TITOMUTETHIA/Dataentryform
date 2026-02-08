1348777e9a896aa9caf5d022c61a6e032de9cec7
TITOMUTETHIA <titomutethia@gmail.com>
2026-02-08 20:28:53 +0300

    edit button

--- WorkTicketApp/obj/Debug/net8.0/WorkTicketApp.AssemblyInfo.cs
+++ WorkTicketApp/obj/Debug/net8.0/WorkTicketApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorkTicketApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19afd6952c71e23a2411481d08848aa932c0731d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f7e19e0f96edf3310367eea09ccd19ee39bfca9")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorkTicketApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorkTicketApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
